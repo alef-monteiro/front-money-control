701c6eb18f9e15437e81d9ac555418aa81a77626
alef-monteiro <monteiro.alef0@gmail.com>
2025-01-18 12:28:56 -0300

    feat: Implements login's token.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {LandingPageComponent} from './pages/landing-page/landing-page.component';
 import {LoginComponent} from './pages/login/login.component';
 import {RegisterComponent} from './pages/register/register.component';
+import {DefaultHomeLayoutComponent} from './components/default-home-layout/default-home-layout.component';
 
 export const routes: Routes = [
   {
@@ -19,5 +20,9 @@ export const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'home',
+    component: DefaultHomeLayoutComponent
   }
 ];
--- src/shared/models/model-base.ts
+++ src/shared/models/model-base.ts
@@ -4,3 +4,5 @@ export interface ModelBase {
     modified_at: Date;
     active: boolean;
 }
+
+
