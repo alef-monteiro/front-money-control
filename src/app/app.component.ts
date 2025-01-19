ddc26a45c99e55ffb9c4d635b56218022f21fcdd
alef-monteiro <monteiro.alef0@gmail.com>
2025-01-19 14:14:13 -0300

    feat: Removing flowbite.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {initFlowbite} from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +7,6 @@ import {initFlowbite} from 'flowbite';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
   title = 'money-control';
-
-  ngOnInit(): void {
-    initFlowbite();
-  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./layouts/**/*.html",
-    "./content/**/*.md",
-    "./content/**/*.html",
-    "./src/**/*.js"],
-
-  safelist: [
-    'w-64',
-    'w-1/2',
-    'rounded-l-lg',
-    'rounded-r-lg',
-    'bg-gray-200',
-    'grid-cols-4',
-    'grid-cols-7',
-    'h-6',
-    'leading-6',
-    'h-9',
-    'leading-9',
-    'shadow-lg'
-  ],
-
+  content: ["./src/**/*.{html,js}"],
   darkMode: 'class',
   theme: {
     extend: {
@@ -76,9 +57,7 @@ module.exports = {
       ]
     }
   },
-  plugins: [
-    require('flowbite/plugin') // add this line
-  ],
+  plugins: [],
 }
 
 
